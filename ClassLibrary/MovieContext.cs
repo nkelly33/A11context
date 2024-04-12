d729ae4e3a1f06666b11a92ead67e9b6cc7aa598
noah kelly <noahjameskelly@yahoo.com>
2024-04-11 20:21:24 -0500

    Add project files.

---
+++ ClassLibrary/MovieContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    internal class MovieContext : DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(
+                configuration.GetConnectionString("MovieContext")
+            );
+        }
+    }
+}
---
+++ Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Movie
+{
+	public Movie()
+	{
+		public string Id;
+		public string Name;
+	public string Description;
+
+	}
+}
