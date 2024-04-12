9262ade782edc143c22803e9f5bdf1c7dff1d577
noah kelly <noahjameskelly@yahoo.com>
2024-04-11 20:39:32 -0500

    added getters and setters

--- Movie.cs
+++ Movie.cs
@@ -4,9 +4,9 @@ public class Movie
 {
 	public Movie()
 	{
-		public string Id;
-		public string Name;
-	public string Description;
+		public string Id {  get; set; }
+		public string Name { get; set; }
+    public string Description { get; set; }
 
-	}
+}
 }
